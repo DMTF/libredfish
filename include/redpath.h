599051a4999e1fd86d55e7cc8c81ea99490e208b
Mike Raineri <michael.raineri@dell.com>
2018-03-23 10:18:33 -0400

    Corrected link to license in source files

--- include/redpath.h
+++ include/redpath.h
@@ -1,7 +1,7 @@
 //----------------------------------------------------------------------------
 // Copyright Notice:
 // Copyright 2017 Distributed Management Task Force, Inc. All rights reserved.
-// License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/libredfish/LICENSE.md
+// License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/libredfish/blob/master/LICENSE.md
 //----------------------------------------------------------------------------
 #ifndef _REDPATH_H_
 #define _REDPATH_H_
